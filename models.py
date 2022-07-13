6acc601259e072f8b5a3a9a4590d34e273b6f603
Abhirikshma Nandi <abhirikshma14@gmail.com>
2022-07-13 09:14:00 +0200

    Improved Clustering
    updated model

--- models.py
+++ models.py
@@ -1,31 +1,18 @@
-import torch
-import awkward as ak
-import numpy as np
-import matplotlib.pyplot as plt
-import pickle
-import torch
-from torch import nn
-import torch.nn.functional as F
-import torch_geometric.transforms as T
-from torch_geometric.nn import EdgeConv
-import torch_geometric.utils as Utils
-from torch_geometric.data import Data
-from torch_geometric.loader import DataLoader
-import torch_geometric.utils as Utils
-
-
 class GraphNet(nn.Module):
-    def __init__(self, input_dim = 8, hidden_dim = 64, output_dim = 1, aggr = 'add', niters = 3):
+    def __init__(self, input_dim = 12, hidden_dim = 64, output_dim = 1, aggr = 'add', niters = 4):
         super(GraphNet, self).__init__()
         
         # transform to latent space
         self.inputnetwork = nn.Sequential(
             nn.Linear(input_dim, hidden_dim),
             nn.BatchNorm1d(hidden_dim),
-            nn.Tanh(),
-            nn.Dropout(0.2),
+            nn.ELU(),
+#             nn.Dropout(0.2),
             nn.Linear(hidden_dim, hidden_dim),
-            nn.ReLU()
+            nn.ReLU(),
+#             nn.Dropout(0.2),
+#             nn.Linear(2*hidden_dim, hidden_dim),
+#             nn.ReLU()
         )
         
         # to compute messages
@@ -43,6 +30,9 @@ class GraphNet(nn.Module):
         # edge features from node embeddings for classification
         self.edgenetwork = nn.Sequential(
             nn.Linear(2 * hidden_dim, hidden_dim),
+            nn.ReLU(),
+            nn.Dropout(0.2),
+            nn.Linear(hidden_dim, hidden_dim),
             nn.ReLU(),
             nn.Dropout(0.2),
             nn.Linear(hidden_dim, output_dim),
@@ -69,19 +59,3 @@ class GraphNet(nn.Module):
         #print(f"score size {(H[src] * H[dst]).sum(dim=-1)}")
         #return (H[src] * H[dst]).sum(dim=-1)
         return self.edgenetwork(torch.cat([H[src], H[dst]], dim=-1)).squeeze(-1)
-    
-# focal loss
-def focal_loss(bce_loss, targets, gamma, alpha):
-    """Binary focal loss, mean.
-
-    Per https://discuss.pytorch.org/t/is-this-a-correct-implementation-for-focal-loss-in-pytorch/43327/5 with
-    improvements for alpha.
-    :param bce_loss: Binary Cross Entropy loss, a torch tensor.
-    :param targets: a torch tensor containing the ground truth, 0s and 1s.
-    :param gamma: focal loss power parameter, a float scalar.
-    :param alpha: weight of the class indicated by 1, a float scalar.
-    """
-    p_t = torch.exp(-bce_loss)
-    alpha_tensor = (1 - alpha) + targets * (2 * alpha - 1)  # alpha if target = 1 and 1 - alpha if target = 0
-    f_loss = alpha_tensor * (1 - p_t) ** gamma * bce_loss
-    return f_loss.mean()
